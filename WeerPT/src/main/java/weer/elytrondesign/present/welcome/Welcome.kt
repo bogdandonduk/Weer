626f8b2d96903db4d15d6f0586b8e8edf9269c90
Bogdan <bogdan.donduk@gmail.com>
2020-08-26 16:48:10 +0700

    Fourth Commit

--- WeerPT/src/main/java/weer/elytrondesign/present/welcome/Welcome.kt
+++ WeerPT/src/main/java/weer/elytrondesign/present/welcome/Welcome.kt
@@ -8,10 +8,12 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import weer.elytrondesign.databinding.FragmentWelcomeBinding
+import weer.elytrondesign.databinding.FragmentWelcomePageBinding
 
 class Welcome() : Fragment() {
     companion object {
         lateinit var binding: FragmentWelcomeBinding
+        lateinit var pageBinding: FragmentWelcomePageBinding
 
         fun getInstance() : Welcome {
             return Welcome()
@@ -24,26 +26,35 @@ class Welcome() : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentWelcomeBinding.inflate(inflater, container, false)
-
-        Home.binding.homeTopBarL.visibility = View.GONE
+        
         return binding.root
     }
 
-    class ScreenOne() : Fragment() {
+    class PageOne() : Fragment() {
         companion object {
+            lateinit var binding: FragmentWelcomePageBinding
+        }
 
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            binding = FragmentWelcomePageBinding.inflate(inflater, container, false)
+
+            return binding.root
         }
     }
 
-    class ScreenTwo() : Fragment() {
+    class PageTwo() : Fragment() {
         companion object {
-
+            lateinit var binding: FragmentWelcomePageBinding
         }
     }
 
-    class ScreenThree() : Fragment() {
+    class PageThree() : Fragment() {
         companion object {
-
+            lateinit var binding: FragmentWelcomePageBinding
         }
     }
 
