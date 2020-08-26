aeefc2eabee0f6c338d3bac8633cce7386040d3f
Bogdan <bogdan.donduk@gmail.com>
2020-08-26 21:01:56 +0700

    Fourth Commit

--- WeerPT/src/main/java/weer/elytrondesign/present/welcome/Home.kt
+++ WeerPT/src/main/java/weer/elytrondesign/present/welcome/Home.kt
@@ -1,9 +1,11 @@
 package weer.elytrondesign.present.welcome
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.fragment.app.FragmentManager
 import weer.elytrondesign.core.Core
 import weer.elytrondesign.databinding.ActivityHomeBinding
@@ -27,6 +29,20 @@ class Home : AppCompatActivity() {
         setContentView(binding.root)
 
         fm = supportFragmentManager
+
+        Core.loadTheme(arrayOf(Core.Companion.ThemedView(
+            binding.homeRl,
+            object : Core.Companion.ViewTransformer {
+                override fun transform(view: View) {
+                    binding.homeRl.setBackgroundColor(Color.WHITE)
+                }
+            },
+            object : Core.Companion.ViewTransformer {
+                override fun transform(view: View) {
+                    binding.homeRl.setBackgroundColor(Color.BLACK)
+                }
+            }
+        )))
 
         if(Core.getPreference(Core.PK_FIRST_LAUNCH, true) as Boolean) {
             Core.loadFragment(Welcome.getInstance(), binding.homeContentL.id)
